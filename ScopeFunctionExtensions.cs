6f238a3d715fa58830c22f2dcda297eafdc57e47
Ivan Koshelev <koshelew@gmail.com>
2022-12-13 13:14:45 +0100

    mostly finished

--- ScopeFunctionExtensions.cs
+++ ScopeFunctionExtensions.cs
@@ -1,22 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Object.Extensions.ScopeFunction
 {
     public static class ScopeFunctionExtensions
     {
-        public static TResp Map<TTarget, TResp>(
-            this TTarget target, 
-            Func<TTarget, TResp> block)
-        {
-            return block(target);
-        }
-
+        /// <summary>
+        /// Applies function to a given object and returns original object
+        /// </summary>
         public static T Apply<T>(
             this T target, 
-            Action<T> block)
+            Action<T> fn)
         {
-            block(target);
+            fn(target);
             return target;
+        }
+
+        /// <summary>
+        /// Applies function to each item in IEnumerable and returns original IEnumerable
+        /// </summary>
+        public static IEnumerable<T> ApplyForEach<T>(
+            this IEnumerable<T> target,
+            Action<T> fn)
+        {
+            foreach (var item in target)
+            {
+                fn(item);
+            }
+            return target;
+        }
+
+        /// <summary>
+        /// Applies function to a given object and returns that functions returns that function return.
+        /// Analog of |> (pipe-forward) operator in F#.
+        /// </summary>
+        public static TResp Map<TTarget, TResp>(
+            this TTarget target, 
+            Func<TTarget, TResp> fn)
+        {
+            return fn(target);
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Scope functions
+
+Functions `Apply`, `ApplyForEach` and `Map` allow you to have side-effects or manipulate objects in fluent invocation chains or object initializers.
+
+```csharp
+var permissionsMissingTestContext = new TestContext
+{
+    Users = GetStandardUsers().ApplyForEach(x => x.Permissions = new[0]),
+    SecurityPolicy = GetStandardSecurityPolicy().Apply(x => x.ShowWarningWhenPermissionsMissing = true),
+    AnonymousPageUrl = GetStandardConfig().Map(x => new Url(x.PermissionsMissingScreenUrl))
+    Timeout = GetTestTimeout()?.Map(x => TimeSpan.FromSeconds(x)) ?? TimeSpan.FromSeconds(30)
+}
+```
