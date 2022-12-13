caf0eb95b5dce27ed9dd36de6eeec947e0e748d5
Ivan Koshelev <koshelew@gmail.com>
2022-12-13 11:35:49 +0100

    init

---
+++ ScopeFunctionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Object.Extensions.ScopeFunction
+{
+    public static class ScopeFunctionExtensions
+    {
+        public static TResp Map<TTarget, TResp>(
+            this TTarget target, 
+            Func<TTarget, TResp> block)
+        {
+            return block(target);
+        }
+
+        public static T Apply<T>(
+            this T target, 
+            Action<T> block)
+        {
+            block(target);
+            return target;
+        }
+    }
+}
